185d5aee53ee1b6057375434bdfff24a9894cd9e
erayk <eraykaya3030@gmail.com>
2024-01-13 15:37:54 +0300

    Update Android manifest and build.gradle, add Google Maps integration

--- lib/apps/map_app/presentation/pages/home_page.dart
+++ lib/apps/map_app/presentation/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -6,12 +7,20 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Blood Donations'),
-      ),
-      body: const Center(
-        child: Text("lorem ipsum"), 
-      ),
-    );
+        appBar: AppBar(
+          title: const Text('Blood Donations'),
+        ),
+        body: GoogleMap(
+          initialCameraPosition: const CameraPosition(
+            target: LatLng(-33.86, 151.20),
+            zoom: 11.0,
+          ),
+          markers: {
+            const Marker(
+              markerId: MarkerId('Sydney'),
+              position: LatLng(-33.86, 151.20),
+            )
+          },
+        ));
   }
 }
